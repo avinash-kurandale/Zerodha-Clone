a5050e377593e5253acaa6eaf3ccf46112c25571
avinash-kurandale <avinashkurandale002@gmail.com>
2025-06-28 02:01:43 +0530

    Home page completed

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-4'>
+                    <h1>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India.
+                         Flat fees and no hidden charges.</p>
+
+                    <a href="#"  style={{ textDecoration: "none" }}>
+                        See pricing{" "}
+                        <i className="fa-solid fa-arrow-right" style={{ color: "#74C0FC" }}></i>
+                        </a>     
+
+                </div>
+
+
+                <div className='col-2'>
+
+                </div>
+
+
+                <div className='col-6'>
+                            <div className='row text-center border'>
+                                <div className='col border p-3 '>
+                                    <h1>₹0</h1>
+                                    <p>Free equity delivery and direct mutual funds</p>
+                                </div>
+                                <div className='col p-3 '>
+                                    <h1>₹20</h1>
+                                    <p>Intraday and F&O</p>
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -4,29 +4,29 @@ function Stats() {
     return ( 
         <div className='container mt-5 mb-5' >
             <div className='row'>
-                <div className='col-6 row-6 p-5'>
+                <div className='col-6 row-6 p-3'>
                         <div className='mt-5 mb-5 '>
                             <div>
-                                <h1>Trust with confidence</h1>
+                                <h1 className='fs-1 mb-5'>Trust with confidence</h1>
 
-                                <h3>Customer-first always</h3>
-                                <p>That's why 1.3+ crore customers trust Zerodha with ₹3.5+
+                                <h3 className='fs-4'>Customer-first always</h3>
+                                <p className='text-muted'>That's why 1.3+ crore customers trust Zerodha with ₹3.5+
                                      lakh crores worth of equity investments.
                                 </p>
 
-                                <h3>No spam or gimmicks</h3>
-                                <p>
+                                <h3 className='fs-4'>No spam or gimmicks</h3>
+                                <p className='text-muted'>
                                     No gimmicks, spam, "gamification", or annoying push notifications. 
                                     High quality apps that you use at your pace, the way you like.
                                 </p>
-                                <h3>The Zerodha universe</h3>
-                                <p>
+                                <h3 className='fs-4'>The Zerodha universe</h3>
+                                <p className='text-muted'>
                                     Not just an app, but a whole ecosystern. Our investments 
                                     in 30+ fintech startups offer you tailored services specific 
                                     to your needs.
                                 </p>
-                                <h3>Do better with money</h3>
-                                <p>
+                                <h3 className='fs-4'>Do better with money</h3>
+                                <p className='text-muted'>
                                     With initiatives like Nudge and Kill Switch, we don't
                                      just facilitate transactions, but actively help you do better
                                       with your money.
@@ -38,6 +38,7 @@ function Stats() {
                 </div>
                 <div className='col-6 row-6 p-5'>
                     <img src='media\images\ecosystem.png'  alt='ecosystem img' style={{width:"90%"}}></img>
+
                     <div className='text-center'>
 
                         <a href="#" className="mx-5" style={{ textDecoration: "none" }}>
