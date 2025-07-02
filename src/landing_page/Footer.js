6a36e28021880d70e646cb44a63cfc50ad2cbad3
avinash-kurandale <avinashkurandale002@gmail.com>
2025-07-03 01:03:49 +0530

    Landing page Frontend work done

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -2,29 +2,109 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="container row">
-      <div className="row">
-        <div className="col">
-                
-                <div className="">
-                    <img src="media\images\logo.svg" style={{width:"15%"}} alt="logo" />
-                    <p>@ 2010-2024, Not Zerodha Broking Ltd. All rights reserved.</p>
-                    <ul>
-                        <ol><i class="fa-brands fa-instagram"></i></ol>
-                        <ol><i class="fa-brands fa-telegram"></i></ol>
-                        <ol><i class="fa-brands fa-whatsapp"></i></ol>
-                        <ol><i class="fa-brands fa-facebook"></i></ol>
-                    </ul>
-                </div>
-                <div className="col"></div>
-                <div className="col"></div>
-                <div className="col"></div>
-            
-        
 
+    <footer className="" style={{backgroundColor:"#e9ecef	"}}>
+    <div className="container border-top mt-5">
+      <div className="row mt-5">
+        <div className="col">
+          <div className="">
+            <img
+              src="media\images\logo.svg"
+              style={{ width: "50%" }}
+              alt="logo"
+            />
+            <p className="text-muted fs-15">@ 2010-2024, Not Zerodha Broking Ltd. All rights reserved.</p>
+            <ul class="d-flex list-unstyled gap-3 fa-2x">
+              <li><i class="fa-brands fa-instagram"></i></li>
+              <li><i class="fa-brands fa-telegram"></i></li>
+              <li><i class="fa-brands fa-whatsapp"></i></li>
+              <li><i class="fa-brands fa-facebook"></i></li>
+            </ul>
+
+          </div>
+        </div>
+        <div className="col foot-a ">
+          <p>Company</p>
+          <a href=" ">Products</a> <br />
+          <a href=" ">About</a> <br />
+          <a href=" ">Referral programme</a>
+          <br />
+          <a href=" ">Careers</a>
+          <br />
+          <a href=" ">Pricing</a> <br />
+          <a href=" ">Zerodha</a>
+          <br />
+          <a href=" ">Press & media</a>
+          <br />
+          <a href=" ">Zerodha cares</a>
+          <br />
+        </div>
+        <div className="col foot-a ">
+          <p>Support</p>
+          <a href=" ">Contact us </a> <br />
+          <a href=" ">Support portal</a> <br />
+          <a href=" ">How to file a complaint?</a> <br />
+          <a href=" ">Status of your complaints</a> <br />
+          <a href=" ">Bulletin</a> <br />
+          
+        </div>
+        <div className="col foot-a ">
+          <p>Account</p>
+          <a href=""> Open demat account</a> <br />
+          <a href=""> Minor demat account</a> <br />
+          <a href=""> NRI demat account</a> <br />
+         
+        </div>
+      </div>
+      <div className="row">
+        <div className="mt-5 fs-10 text-muted">
+        <p>
+          Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration
+          no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha
+          Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading
+          through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI
+          Registration no.: INZ000038238 Registered Address: Zerodha Broking
+          Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public
+          School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For
+          any complaints pertaining to securities broking please write to
+          complaints@zerodha.com, for DP related to dp@zerodha.com. Please
+          ensure you carefully read the Risk Disclosure Document as prescribed
+          by SEBI | ICF
+        </p>
+        <p>
+          Procedure to file a complaint on SEBI SCORES: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances
+        </p>
+        <p>
+          Attention investors: 1) Stock brokers can accept securities as margins
+          from clients only by way of pledge in the depository system w.e.f
+          September 01, 2020. 2) Update your e-mail and phone number with your
+          stock broker / depository participant and receive OTP directly from
+          depository on your e-mail and/or mobile number to create pledge. 3)
+          Check your securities / MF / bonds in the consolidated account
+          statement issued by NSDL/CDSL every month. "Prevent unauthorised
+          transactions in your account. Update your mobile numbers/email IDs
+          with your stock brokers. Receive information of your transactions
+          directly from Exchange on your mobile/email at the end of the day.
+          Issued in the interest of investors. KYC is one time exercise while
+          dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+          create a ticket here.
+        </p>
         </div>
       </div>
     </div>
+    </footer>
   );
 }
 
